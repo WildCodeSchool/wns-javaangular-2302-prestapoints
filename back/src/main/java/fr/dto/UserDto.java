eddc9049345c9c921c1acfc6a78dae2a56fc31fc
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-04-21 21:11:35 +0200

    end of init

---
+++ back/src/main/java/fr/dto/UserDto.java
@@ -0,0 +1,13 @@
+package fr.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    
+    private Long id;
+    private String firstname;
+
+}
---
+++ back/src/main/java/fr/service/UserService.java
@@ -0,0 +1,29 @@
+package fr.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.dto.UserDto;
+import fr.mapper.UserToDtoMapper;
+import fr.model.User;
+import fr.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserToDtoMapper userToDtoMapper;
+
+    public void createUser(UserDto userDto) {
+        User user = userToDtoMapper.convertToEntity(userDto);
+        userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.getById(id);
+    }
+}
