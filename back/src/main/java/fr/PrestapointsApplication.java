6ae5d18cfe659237065fce98519b61a38aed4724
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-04-22 00:52:27 +0200

    added fixtures

--- back/src/main/java/fr/PrestapointsApplication.java
+++ back/src/main/java/fr/PrestapointsApplication.java
@@ -1,11 +1,16 @@
 package fr;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import com.github.javafaker.Faker;
 
 @SpringBootApplication
@@ -20,32 +25,86 @@ public class PrestapointsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		userFixture(10);
+		Faker faker = new Faker();
+		// NE JAMAIS METTRE L'ID
+		// remplir les noms de colonnes MySQL
+		List<String> userColumns = Arrays.asList(
+				"firstname",
+				"lastname");
+
+		// remplir les fakers des colonnes précédentes
+		Supplier<?>[] userSuppliers = new Supplier[] {
+				() -> faker.name().firstName(),
+				() -> faker.name().lastName()
+		};
+
+		// mettre le nom de la table entre double ET simple quote
+		// mettre le nombre de ligne souhaitée
+		fixtures("'user'", 10, userColumns, userSuppliers);
+		/***********************************************/
+		List<String> testColumns = Arrays.asList(
+				"role",
+				"avion",
+				"date");
+
+		Supplier<?>[] testSuppliers = new Supplier[] {
+				() -> faker.name().prefix(),
+				() -> faker.starTrek().character(),
+				() -> faker.date().birthday()
+		};
+		fixtures("'test'", 100, testColumns, testSuppliers);
+
+		/***********************************************/
+	}
+/********************************************************************************************/
+	// DO NOT TOUCH
+	private void fakingDB(int number, List<String> columns, String tableName, Supplier<?>[] suppliers) {
+		int columnsNumber = columns.size();
+		tableName = tableName.replace("'", "");
+		String insert = "INSERT INTO " + tableName + " (id,";
+		String values = " VALUES (?,";
+
+		for (int i = 0; i < columnsNumber; i++) {
+			if ((i + 1) == columnsNumber) {
+				insert += columns.get(i) + ")";
+				values += "?)";
+			} else {
+				insert += columns.get(i) + ",";
+				values += "?,";
+			}
+		}
+		for (int id = 1; id <= number; id++) {
+			Object[] params = new Object[suppliers.length + 1];
+			params[0] = id;
+			for (int i = 0; i < suppliers.length; i++) {
+				params[i + 1] = suppliers[i].get();
+			}
+			jdbcTemplate.update(insert + values, params);
+		}
 	}
 
-
-	private void userFixture(int number) {
-		if (jdbcTemplate.queryForObject(
-			"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'user'",
-				Integer.class) > 0) {
-
+	private void fixtures(String tableName, int number, List<String> columns, Supplier<?>[] suppliers) {
+		if (isTableExist(tableName)) {
 			// La table existe, on la vide
-			jdbcTemplate.update("DELETE FROM user");
-
+			deleteTable(tableName);
 			// On ajoute des données fictives
-			Faker faker = new Faker();
-			for (int id = 1; id <= number; id++) {
-				String firstname = faker.name().firstName();
-				String lastname = faker.name().lastName();
-			
-				//on insère dans la BDD
-				jdbcTemplate.update(
-					"INSERT INTO user (id, firstname, lastname) VALUES (?, ?, ?)",
-						id, firstname, lastname);
-			}
+			fakingDB(number, columns, tableName, suppliers);
+
 		} else {
 			// La table n'existe pas, on renvoie une erreur
-			throw new RuntimeException("La table 'user' n'existe pas.");
+			throw new RuntimeException("La table " + tableName + " n'existe pas.");
 		}
+	}
+
+	private boolean isTableExist(String tableName) {
+
+		return jdbcTemplate.queryForObject(
+				"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = " + tableName,
+				Integer.class) > 0;
+	}
+
+	private void deleteTable(String tableName) {
+		tableName = tableName.replace("'", "");
+		jdbcTemplate.update("DELETE FROM " + tableName);
 	}
 }
---
+++ back/src/main/java/fr/model/Test.java
@@ -0,0 +1,24 @@
+package fr.model;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Test {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String role;
+    private String avion;
+    private Date date;
+}
+
