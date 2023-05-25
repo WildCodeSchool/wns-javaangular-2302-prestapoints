9dd403d44f2833020e69fe690d155d68746c5c0e
Vannou28 <vannier.aurelien2@gmail.com>
2023-05-25 16:32:27 +0200

    LongToInteger

--- back/src/main/java/fr/repository/RegistrationRepository.java
+++ back/src/main/java/fr/repository/RegistrationRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import fr.entity.Registration;
 
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
     
     @Query("SELECT r FROM Registration r WHERE r.prestation.id = :id")
-    Iterable<Registration> findAllByPrestationId(@Param("id") Long id);
+    Iterable<Registration> findAllByPrestationId(@Param("id") Integer id);
 
     @Query("SELECT r, p FROM Registration r JOIN Prestation p")
     Iterable<Registration> findAllPrestationWithComment();
