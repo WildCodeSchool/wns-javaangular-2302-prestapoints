00db6d6e97b21dad4f23bc025f83290ddb210642
Luc Jaubert <j.luc@obvy-app.com>
2023-06-30 09:50:06 +0200

    Revert "changes footer, improvement"
    This reverts commit 581923d45e05f4b4ba069add5cab8f8cd015e322.

--- front/src/app/shared/components/footer/footer.component.ts
+++ front/src/app/shared/components/footer/footer.component.ts
@@ -6,35 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  tags: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.tags = this.getTags();
   }
 
-  getTags(): string[] {
-    return [
-      'Ameublement',
-      'Animaux',
-      'Artisanat',
-      'Bijoux',
-      'Bricolage',
-      'CréationMeubles',
-      'DressageChiens',
-      'Poterie',
-      'Peinture',
-      'Couture',
-      'Jardinage',
-      'Cuisine',
-      'Photographie',
-      'Danse',
-      'Théâtre',
-      'Yoga',
-      'Dessin',
-      'Musique',
-      'Fitness'
-    ];
-  }
 }
