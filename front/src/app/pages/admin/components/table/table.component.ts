67bdb3b9a593e73a838ba91df969cc1b3c28b47b
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-07-03 22:48:10 +0200

    dashboard list users angular

---
+++ front/src/app/pages/admin/components/table/table.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/model/user';
+import { UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent {
+  @Input()
+  users?: User[];
+
+}
