dbd427657190a47e034f1ca6c539c8056cbbdb29
Vannou28 <vannier.aurelien2@gmail.com>
2023-07-07 11:40:37 +0200

    fix reservation componenent

--- front/src/app/shared/model/prestation.ts
+++ front/src/app/shared/model/prestation.ts
@@ -7,17 +7,17 @@ export class Prestation {
 
     public id? : number;
     public title? : string;
-    public duration? : string;
-    public addPoint? : string;
-    public dateStart? : string;
-    public dateEnd? : string;
+    public duration? : number;
+    public addPoint? : number;
+    public dateStart? : number;
+    public dateEnd? : number;
     public state? : string;
     public description? : string;
-    public maxUser? : string;
+    public maxUser? : number;
     public image? : Image[];
     public type?: Type;
     public location?: Location;
-    public placeAvailable?: string;
+    public placeAvailable?: number;
     public littleDescription?: string;
     public practicalInformation?: string;
     public language?: string;
@@ -27,6 +27,76 @@ export class Prestation {
 
     constructor(){}
 
-
+    getId(): number | undefined {
+        return this.id;
+      }
+    
+      getTitle(): string | undefined {
+        return this.title;
+      }
+    
+      getDuration(): string | undefined {
+        return this.duration?.toString();
+      }
+    
+      getAddPoint(): string | undefined {
+        return this.addPoint?.toString();
+      }
+    
+      getDateStart(): string | undefined {
+        return this.dateStart?.toString();
+      }
+    
+      getDateEnd(): string | undefined {
+        return this.dateEnd?.toString();
+      }
+    
+      getState(): string | undefined {
+        return this.state;
+      }
+    
+      getDescription(): string | undefined {
+        return this.description;
+      }
+    
+      getMaxUser(): string | undefined {
+        return this.maxUser?.toString();
+      }
+    
+      getImage(): Image[] | undefined {
+        return this.image;
+      }
+    
+      getType(): Type | undefined {
+        return this.type;
+      }
+    
+      getLocation(): Location | undefined {
+        return this.location;
+      }
+    
+      getPlaceAvailable(): number | undefined {
+        return this.placeAvailable;
+      }
+    
+      getLittleDescription(): string | undefined {
+        return this.littleDescription;
+      }
+    
+      getPracticalInformation(): string | undefined {
+        return this.practicalInformation;
+      }
+    
+      getLanguage(): string | undefined {
+        return this.language;
+      }
+    
+      getPersonalInfos(): string | undefined {
+        return this.personalInfos;
+      }
+    
+      getRegistration(): Registration[] | undefined {
+        return this.registration;
+      }
 
 }
