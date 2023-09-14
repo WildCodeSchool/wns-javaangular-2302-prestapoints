26426631b97f46d5e437fcb8d3e734df106b4e08
Vannou28 <vannier.aurelien2@gmail.com>
2023-09-14 11:20:07 +0200

    load fixtures location

--- back/src/main/java/fr/fixture/LocationFixtures.java
+++ back/src/main/java/fr/fixture/LocationFixtures.java
@@ -1,11 +1,15 @@
 package fr.fixture;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.github.javafaker.Faker;
 import fr.entity.Location;
+import fr.entity.User;
 import fr.enums.TablesEnum;
 import fr.repository.LocationRepository;
+import fr.repository.UserRepository;
 
 
 @Component
@@ -15,6 +19,8 @@ public class LocationFixtures {
     private Fixtures fixtures;
     @Autowired
     LocationRepository locationRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public void prepareFixtures() {
 
@@ -26,12 +32,21 @@ public class LocationFixtures {
             Integer numberOfLigne = 10;
             for (int i = 0; i < numberOfLigne; i++) {
                 location.setId(i);
-                String numeroDeRue = faker.address().buildingNumber();
+
+                String numeroDeRue = String.valueOf(faker.number().numberBetween(1,300));
                 String rue = faker.address().streetName();
                 location.setAddress(numeroDeRue + " rue "+ rue);
-                location.setPostalCode(faker.address().countryCode());
-                location.setAddressInformation("N/C");
+
+                location.setPostalCode(String.valueOf(faker.number().numberBetween(10000,95100)));
+                location.setAddressNumber(numeroDeRue);
                 location.setCity(faker.address().cityName());
+
+                Optional<User> user = userRepository.findById(i+1);
+                String userFirstName = user.get().getFirstname();
+                String tramStopName1 =faker.name().fullName();
+                String tramStopName2 =faker.name().fullName();
+                location.setAddressInformation("L'atelier de "+ userFirstName + " se trouve dans la rue " + rue + ", à quelques pas de l'arrêt de Tram "+ tramStopName1 +" (Tram A) et "+ tramStopName2 + " (BUS C).");
+                
                 locationRepository.save(location);
             }
         }
