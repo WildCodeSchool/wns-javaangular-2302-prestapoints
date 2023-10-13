98285b03f565ed549ddb951cbd2ff6de754071a4
Vannou28 <vannier.aurelien2@gmail.com>
2023-10-13 11:38:09 +0200

    Bp suivant et précédent

--- front/src/app/pages/prestation/prestation-formulaire/prestation-formulaire.component.ts
+++ front/src/app/pages/prestation/prestation-formulaire/prestation-formulaire.component.ts
@@ -18,101 +18,113 @@ export class PrestationFormulaireComponent {
     prestation!: Prestation;
     location!: Location;
     categories!: Category[];
-
+    isLoaded: boolean = false;
     prestationFormBasic!: FormGroup;
     prestationFormDescription!: FormGroup;
     prestationFormLocation!: FormGroup;
     prestationFormMedia!: FormGroup;
     prestationFormCharacteristic!:  FormGroup;
+    pageNumber: number = 0;
 
 
-
-    formFieldsBasic: { name: string; label: string; type: string; validationMessage: string }[] = [
-        { name: 'title', label: 'Titre', type: 'text', validationMessage: 'Le champs est limité à 50 caractères.' },
-        { name: 'type', label: 'Type', type: 'text', validationMessage: 'Le champs est limité à 50 caractères.' },
-        { name: 'category', label: 'Catégorie', type: 'text', validationMessage: 'Le champs est limité à 50 caractères.' },
+    formFieldsBasic: { name: string; label: string; type: string; placeholder: string; validationMessage: string }[] = [
+        { name: 'type', label: 'Type', type: 'text', placeholder: "ex : Dressage de chiens", validationMessage: 'Le champs est limité à 50 caractères.' },
+        { name: 'title', label: 'Titre', type: 'text', placeholder: "ex : Apprenez à dresser votre chien avec notre expert canin", validationMessage: 'Le champs est limité à 50 caractères.' },
     ];
 
-    formFieldsCharacteristic: { name: string; label: string; type: string; validationMessage: string }[] = [
-        { name: 'dateStart', label: 'Date de début', type: 'date', validationMessage: 'La date doit être sous forme jj/mm/aaaa et supérieur à aujourdui.' },
-        { name: 'heureDebutPrestation', label: 'Heure de début', type: 'text', validationMessage: "On acceepte que des nombres" },
-        { name: 'duration', label: 'Durée de la prestation en Heure', type: 'number', validationMessage: "L'horraire n'est pas conforme hh:mm." },
-        { name: 'maxUser', label: 'Nombre maximal d\'utilisateurs', type: 'number', validationMessage: 'L\'information du nombre de participants maximal est obligatoire.' }
+    formFieldsCharacteristic: { name: string; label: string; type: string; placeholder: string; validationMessage: string }[] = [
+        { name: 'dateStart', label: 'Date de début', type: 'date', placeholder: "", validationMessage: 'La date doit être sous forme jj/mm/aaaa et supérieur à aujourdui.' },
+        { name: 'timeStart', label: 'Heure de début', type: 'text', placeholder: "ex : 16:00", validationMessage: "On acceepte que des nombres" },
+        { name: 'duration', label: 'Durée de la prestation en Heure', type: 'number', placeholder: "ex : 3", validationMessage: "L'horraire n'est pas conforme hh:mm." },
+        { name: 'maxUser', label: 'Nombre maximal d\'utilisateurs', type: 'number', placeholder: "ex : 6", validationMessage: 'L\'information du nombre de participants maximal est obligatoire.' }
     ];
 
-    formFieldsDescription: { name: string; label: string; type: string; validationMessage: string }[] = [
-        { name: 'practicalInformation', label: 'Informations pratique', type: 'text', validationMessage: 'Le champs est limité à 255 caractères.' },
-        { name: 'description', label: 'Description', type: 'textarea', validationMessage: 'Une description de la prestation est obligatoire.' },
-        { name: 'littleDescription', label: 'Petite description', type: 'text', validationMessage: 'Le champs est limité à 50 caractères.' },
-        { name: 'language', label: 'Durée de la prestation', type: 'text', validationMessage: "Le champs est limité à 50 caractères." },
-        { name: 'personalInfos', label: 'Quelques informations personnelles', type: 'text', validationMessage: 'Le champs est limité à 255 caractères.' },
+    formFieldsDescription: { name: string; label: string; type: string; placeholder: string; validationMessage: string }[] = [
+        { name: 'practicalInformation', label: 'Informations pratique', type: 'text', placeholder: "ex : Les chiens admis sont des chiens dressés ...", validationMessage: 'ex : Le champs est limité à 255 caractères.' },
+        { name: 'description', label: 'Description', type: 'textarea', placeholder: "ex : Découvrez les techniques de dressage de chiens et renforcez ...", validationMessage: 'Une description de la prestation est obligatoire.' },
+        { name: 'littleDescription', label: 'Petite description', type: 'text', placeholder: "ex : Découvrez les techniques de dressage de chiens ...", validationMessage: 'Le champs est limité à 50 caractères.' },
+        { name: 'language', label: 'Durée de la prestation', type: 'text', placeholder: "ex : francais", validationMessage: "Le champs est limité à 50 caractères." },
+        { name: 'personalInfos', label: 'Quelques informations personnelles', type: 'text', placeholder: "ex : Notre expert canin possède une vaste expérience ...", validationMessage: 'Le champs est limité à 255 caractères.' },
     ];
 
-    formFieldsLocation: { name: string; label: string; type: string; validationMessage: string }[] = [
-        { name: 'LocationAddressNumber', label: 'Numéro de la rue', type: 'text', validationMessage: "L'horraire n'est pas conforme hh:mm." },
-        { name: 'LocationAddress', label: 'Nom de la rue', type: 'text', validationMessage: 'Le champs est limité à 50 caractères.' },
-        { name: 'postalCode', label: 'Code postal', type: 'text', validationMessage: 'Le champs doit être de ce format 45000.' },
-        { name: 'city', label: 'Nom de la ville', type: 'text', validationMessage: 'Le champs est limité à 50 caractères.' },
-        { name: 'LocationAddressInformation', label: 'Informations complémentaires sur l\'adresse', type: 'text', validationMessage: 'Le champs est limité à 255 caractères.' }, 
+    formFieldsLocation: { name: string; label: string; type: string; placeholder: string; validationMessage: string }[] = [
+        { name: 'LocationAddressNumber', label: 'Numéro de la rue', type: 'text', placeholder: "ex : 25bis", validationMessage: "L'horraire n'est pas conforme hh:mm." },
+        { name: 'LocationAddress', label: 'Nom de la rue', type: 'text', placeholder: "ex : rue de la Forge", validationMessage: 'Le champs est limité à 50 caractères.' },
+        { name: 'LocationPostalCode', label: 'Code postal', type: 'text', placeholder: "ex : 45000", validationMessage: 'Le champs doit être de ce format 45000.' },
+        { name: 'LocationCity', label: 'Nom de la ville', type: 'text', placeholder: "ex : ORLEANS", validationMessage: 'Le champs est limité à 50 caractères.' },
+        { name: 'LocationAddressInformation', label: 'Informations complémentaires sur l\'adresse', type: 'text', placeholder: "ex : L'atelier de Modesto se trouve dans la rue Gerlach Ports ...", validationMessage: 'Le champs est limité à 255 caractères.' }, 
     ];
 
     constructor(private formBuilder: FormBuilder, private http: HttpClient, private formValidatorsService: FormValidatorsService, private categoryService: CategoryService) {
   
-            this.categoriesSubscription();
-
-        
-
-     }
-  
-    categoriesSubscription() {
         this.categoryService.getCategories().subscribe((categories) => {
             this.categories = categories;
+            this.isLoaded = true;
+            console.log("categories : " + this.categories);
         });
-    }
+     }
+  
 
     ngOnInit(): void {
-        console.log("categories : " + this.categories);
-
+    
         this.prestationFormBasic = this.formBuilder.group({
             title: ['', [Validators.required, Validators.maxLength(50)]],
             type: ['', [Validators.required, Validators.maxLength(50)]],
-            category: ['', [Validators.required, Validators.maxLength(50)]],
-            duration: ['', [Validators.required, ]],
-            dateStart: ['', [Validators.required, this.formValidatorsService.dateValidator()]],
-            maxUser: ['', [Validators.required, Validators.maxLength(3)]]
+            category: [],
         });
 
         this.prestationFormCharacteristic = this.formBuilder.group({
-            duration: ['', [Validators.required, ]],
-            dateStart: ['', [Validators.required, this.formValidatorsService.dateValidator()]],
-            maxUser: ['', [Validators.required, Validators.maxLength(3)]]
+        dateStart: ['', [Validators.required, this.formValidatorsService.dateValidator()]],
+        timeStart: ['', [Validators.required, this.formValidatorsService.timeValidator]],
+            duration: ['', [Validators.required, Validators.maxLength(2)]],
+            maxUser: ['', [Validators.required, Validators.maxLength(3)]],
         });
 
         this.prestationFormDescription = this.formBuilder.group({
-            practicalInformation: ['', Validators.required, Validators.maxLength(255)],
-            description: ['', Validators.required, Validators.maxLength(1000)],
-            littleDescription: ['', Validators.required, Validators.maxLength(255)],
-            language: ['', Validators.required, Validators.maxLength(50)],
-            personalInfos: ['', Validators.required, Validators.maxLength(255)],
+            practicalInformation: ['',[ Validators.required, Validators.maxLength(255)]],
+            description: ['', [Validators.required, Validators.maxLength(1000)]],
+            littleDescription: ['',[ Validators.required, Validators.maxLength(255)]],
+            language: ['', [Validators.required, Validators.maxLength(50)]],
+            personalInfos: ['', [Validators.required, Validators.maxLength(255)]],
         });
 
         this.prestationFormLocation = this.formBuilder.group({
-            LocationCity: ['', Validators.required, Validators.maxLength(50)],
-            LocationPostalCode: ['', Validators.required, this.formValidatorsService.postalCodeValidator(),Validators.maxLength(5)],
-            LocationAddress: ['', Validators.required, Validators.maxLength(50)],
-            LocationAddressNumber: ['', Validators.required, Validators.maxLength(50)],
-            LocationAddressInformation: ['', Validators.required, Validators.maxLength(255)],
+            LocationCity: ['', [Validators.required, Validators.maxLength(50)]],
+            LocationPostalCode: ['', [Validators.required, this.formValidatorsService.postalCodeValidator(),Validators.maxLength(5)]],
+            LocationAddress: ['', [Validators.required, Validators.maxLength(50)]],
+            LocationAddressNumber: ['', [Validators.required, Validators.maxLength(50)]],
+            LocationAddressInformation: ['', [Validators.required, Validators.maxLength(255)]],
         });
 
         this.prestationFormMedia= this.formBuilder.group({
             mediaImage: ['', Validators.required],
             mediaVideo: ['', Validators.required, Validators.maxLength(255)],
         });
-      }
-  
+    }
+
+    onSubmitBpNext(pageNumber: number){
+        /*if (pageNumber == 0 && this.prestationFormBasic.invalid) {
+            return;
+        }
+        if (pageNumber == 1 && this.prestationFormCharacteristic.invalid) {
+            return;
+        }
+        if (pageNumber == 2 && this.prestationFormDescription.invalid) {
+            return;
+        }*/
+
+        
+        this.pageNumber = pageNumber + 1;
+    }
+
+    onSubmitBpPrevious(pageNumber: number){
+        console.log("je suis dedans");
+        this.pageNumber = pageNumber - 1;
+    }
+
     onSubmit() {
         
-        if (this.prestationFormBasic.invalid) {
+        if (this.prestationFormLocation.invalid) {
           return;
         }
 
@@ -125,8 +137,12 @@ export class PrestationFormulaireComponent {
         this.prestation.type = formDataBasic.type;
         this.prestation.category = formDataBasic.category;
 
-        const date = new Date(formDataCharacteristic.dateStart);
+
+        const [jour, mois, annee] = formDataCharacteristic.dateStart.split('/');
+        const [heures, minutes] = formDataCharacteristic.timeStart.split(':');
+        const date = new Date(annee, mois - 1, jour, heures, minutes); 
         this.prestation.dateStartTimestamps = date.getTime();
+        
         this.prestation.duration = formDataCharacteristic.duration;
         this.prestation.maxUser = formDataCharacteristic.maxUser;
 
