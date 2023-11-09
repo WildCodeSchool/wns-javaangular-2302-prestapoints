9afe9e096d970b28cdff29d05c80c2c7d7cb5be9
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-11-09 16:33:55 +0100

    * in cors

--- back/src/main/java/fr/config/WebSecurityConfig.java
+++ back/src/main/java/fr/config/WebSecurityConfig.java
@@ -94,7 +94,7 @@ public class WebSecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:4200", "https://staging.prestapoints.lille-1.wilders.dev"));
+                Arrays.asList("http://localhost:4200", "https://staging.prestapoints.lille-1.wilders.dev", "**", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList(
